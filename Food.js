96a83e7353b48177e7faad1ceca24df60b1203e3
Elona Rashica <elona.rashica@student.uni-pr.edu>
2021-05-23 16:40:41 +0200

    .

--- Food.js
+++ Food.js
@@ -1,48 +1,36 @@
-function myFunction() {
-    var x = document.getElementById("bmyTopnav");
-    if (x.className === "btopnav") {
-      x.className += " bresponsive";
-    } else {
-      x.className = "btopnav";
-    }
-  } 
-
-
-
-        // Get the modal
-        var modal = document.getElementById('login-id01');
+// function myFunction() {
+//   var x = document.getElementById("bmyTopnav");
+//   if (x.className === "btopnav") {
+//     x.className += " bresponsive";
+//   } else {
+//     x.className = "btopnav";
+// //   }
+// // } 
+// // Get the modal
+// var modal = document.getElementById('login-id01');
         
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
+// //  // Get the modal
+// //  var modal = document.getElementById('login-id01');
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
   
 
 
 
-// Get the modal
-var modal = document.getElementById('signup-id01');
+// // Get the modal
+// var modal = document.getElementById('signup-id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-window.onscroll = function() {myFunction()};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
-var header = document.getElementById("bmyTopnav");
-var sticky = header.offsetTop;
-
-function myFunction() {
-if (window.pageYOffset > sticky) {
-  header.classList.add("sticky");
-} else {
-  header.classList.remove("sticky");
-}
-}
 
 
 function validateForm() {
